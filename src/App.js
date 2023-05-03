baa4fc779184d50cdf1a32e5ac4a73ee90cd204d
unknown <skawldhks@naver.com>
2023-05-03 15:58:36 +0900

    menu2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import Header from './Header';
 import MainImage from './MainImage';
 import { GlobalStyles } from './styles';
-import styled from 'styled-components';
 
-const slyderImage=styled.div`
-  width:1000px;
-  height:100px;
-`
+
 
 function App() {
   return (
@@ -16,7 +12,6 @@ function App() {
       
       {/* 상단 메뉴 */}
       <Header/>
-      <slyderImage>슬라이더 이미지</slyderImage>
       {/*메인 이미지 */}
       <MainImage/>
     </>
--- src/MainImage.jsx
+++ src/MainImage.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { GlobalStyles } from './styles';
 
 const ContainerImage = styled.div`
   width: 100%;
